279c6a408e82f6fba0505eb8a08a4eaa5612c75e
ant253 <antoni.laczny@gmail.com>
2025-02-16 18:59:49 +0000

    Small changes to server.js

--- server.js
+++ server.js
@@ -17,48 +17,54 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Load users from file
 const loadUsers = () => {
-  if (fs.existsSync(USERS_FILE)) {
-    const data = fs.readFileSync(USERS_FILE);
-    return JSON.parse(data);
-  }
-  return [];
+    if (fs.existsSync(USERS_FILE)) {
+        try {
+            const data = fs.readFileSync(USERS_FILE, 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            return [];
+        }
+    }
+    return [];
 };
 
 // Save users to file
 const saveUsers = (users) => {
-  fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
-};
-
-// Middleware to check if user is authenticated
-const authenticate = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.redirect('/login.html');
-  }
-  try {
-    const decoded = jwt.verify(token, 'secret');
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.redirect('/login.html');
-  }
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
 };
 
 // Routes
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
-// Serve the initial setup page if no users exist
+// Root route handler
 app.get('/', (req, res) => {
-  const users = loadUsers();
-  if (users.length === 0) {
-    return res.redirect('/setup.html');
-  }
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    const users = loadUsers();
+    
+    if (users.length === 0) {
+        return res.redirect('/setup.html');
+    }
+    
+    const token = req.cookies.token;
+    if (!token) {
+        return res.redirect('/login.html');
+    }
+    
+    try {
+        jwt.verify(token, 'secret');
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    } catch (err) {
+        res.redirect('/login.html');
+    }
 });
 
+// Make sure users.json exists
+if (!fs.existsSync(USERS_FILE)) {
+    fs.writeFileSync(USERS_FILE, '[]');
+}
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = { loadUsers, saveUsers };
